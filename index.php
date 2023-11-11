37cc81689ead0f405cb7f93abdf98d490eacb6c8
Sebastián Felipe López Wilson <129629599+Sebaslw@users.noreply.github.com>
2023-11-11 15:36:00 -0500

    webphp
    WebPHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("header.php")?>
+ 
+
+
+
+
+
+
+
+<section>
+        <img src="https://imgs.search.brave.com/R3eru81vNpfygRUvIMxpjQCLHtiwXSO957PS5VghS6c/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA2LzQ0LzAzLzI0/LzM2MF9GXzY0NDAz/MjQ0N185Y2NtQXdG/V1ZHdndwT1FWSVQ5/SldwWnU0WmYwQTFN/eS5qcGc" alt="">
+    </section>
+</body>
+</html>
